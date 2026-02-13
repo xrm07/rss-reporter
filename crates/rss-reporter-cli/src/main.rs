0434e2a59da7a31fd772615971a6d24eec140ac1
xrm07 <heyehema@gmail.com>
2026-02-13 16:02:20 +0900

    rssを取得後，stdoutにて出力まで

--- crates/rss-reporter-cli/src/main.rs
+++ crates/rss-reporter-cli/src/main.rs
@@ -1,3 +1,14 @@
+use feed_rs::parser;
+use std::process::Command;
+use std::io::Cursor;
+
 fn main() {
-    println!("Hello, world!");
+    let out = Command::new("curl").arg("https://scour.ing/@xrm07/rss.xml").output().expect("Failed to execute process");
+    if out.status.success() == true {
+        let feed = parser::parse(Cursor::new(out.stdout.as_slice()));
+        println!("{:?}",feed.unwrap().title.unwrap());
+    }else{
+        println!("{:?}",out.stderr);
+        return;
+    }
 }
