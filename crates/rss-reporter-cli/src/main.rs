69befe487910d5fc44f076fea7356689a8e6c4ac
xrm07 <heyehema@gmail.com>
2026-02-18 20:40:18 +0900

    feat:xmlを整形し，標準出力する機能を追加

--- crates/rss-reporter-cli/src/main.rs
+++ crates/rss-reporter-cli/src/main.rs
@@ -1,14 +1,25 @@
-use feed_rs::parser;
 use std::process::Command;
-use std::io::Cursor;
-
+use feed_rs::parser;
 fn main() {
     let out = Command::new("curl").arg("https://scour.ing/@xrm07/rss.xml").output().expect("Failed to execute process");
-    if out.status.success() == true {
-        let feed = parser::parse(Cursor::new(out.stdout.as_slice()));
-        println!("{:?}",feed.unwrap().title.unwrap());
+    if out.status.success(){
+        let xml = String::from_utf8_lossy(&out.stdout).to_string();
+        let feed = parser::parse(xml.as_bytes()).unwrap();
+        let vec_site_ctx = rss_reporter_core::feed_to_site_ctx(feed);
+        println!("{:?}",vec_site_ctx);
+        for ctx in vec_site_ctx{
+            println!("--------------------");
+            println!("{:?}: tag:{}",ctx.article_title.content,ctx.topic_tag);
+            println!("link: {}",ctx.article_url);
+            println!("--------------------");
+        }
     }else{
         println!("{:?}",out.stderr);
         return;
     }
 }
+/*
+std::process::commandによるコマンド実行の返り値，Output．そのエンティティ．stdoutはコマンド実行後によって返されるもの，stderrはstdoutのエラーを受け取るものをもつ．
+これらはVec<u8>で保存される．これらはバイト列なので普通にprintしようとすると数字の羅列が帰ってくる．それを加工するのがString::from_utf8_lossy,これによってutf-8にバイト列を検証しながら変換してくれる．
+
+*/
--- crates/rss-reporter-core/src/lib.rs
+++ crates/rss-reporter-core/src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use feed_rs::model::{Feed, Text};
+
+#[derive(Debug)]
+pub struct site_ctx {
+    pub site_name:String,
+    pub article_title:Text,
+    pub topic_tag:String,
+    pub article_url:String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn feed_to_site_ctx(feed:Feed) -> Vec<site_ctx>{
+    let mut vec_site_ctx:Vec<site_ctx> = Vec::new();
+    for current in feed.entries{
+        let modified = site_ctx{
+            site_name:feed.title.clone().unwrap().content,
+            article_title:current.title.unwrap(),
+            topic_tag:current.categories.get(0)
+                                        .map(|c| c.term.clone())
+                                        .unwrap_or_else(|| "not existed".to_string()),
+            article_url:current.links
+                                        .get(0)
+                                        .map(|l| l.href.clone())
+                                        .unwrap_or_else(|| "".to_string()),
+        };
+        vec_site_ctx.push(modified);
     }
+    vec_site_ctx
 }
